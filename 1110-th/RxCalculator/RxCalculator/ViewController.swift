416f9237dae3cea47d280c6f75566aebf44577ad
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-10 12:18:04 -0800

    implemented a fully FRP functioning calculator with basic addition and subtraction

--- 1110-th/RxCalculator/RxCalculator/ViewController.swift
+++ 1110-th/RxCalculator/RxCalculator/ViewController.swift
@@ -7,21 +7,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var operationSegmentedControl: UISegmentedControl!
-  @IBOutlet weak var firstValueTextField: UITextField!
-  @IBOutlet weak var secondValueTextField: UITextField!
-  @IBOutlet weak var operationLabel: UILabel!
-  @IBOutlet weak var resultLabel: UILabel!
-  
-
-  // MARK: View Controller Lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
+    
+    @IBOutlet weak var operationSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var firstValueTextField: UITextField!
+    @IBOutlet weak var secondValueTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var calculator = Calculator()
+    let disposeBag = DisposeBag()
+    
+    // MARK: View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let firstValueObservable: Observable<Int> = firstValueTextField.rx.text.map { (text : String?) in
+            return Int(text!)!
+        }
+        
+        let secondValueObservable: Observable<Int> = secondValueTextField.rx.text.map { (text: String?) in
+            return Int(text!)!
+        }
+        
+        let operationObservable: Observable<Calculator.Operation> = operationSegmentedControl.rx.value.map { (selectedIndex: Int) in
+            return Calculator.Operation(rawValue: selectedIndex)!
+        }
+        
+        let signObservable : Observable<String> = operationObservable.map { (operation : Calculator.Operation) in
+            return self.calculator.sign(for: operation)
+        }
+        
+        let resultObservable = Observable.combineLatest(operationObservable, firstValueObservable, secondValueObservable) { (operation : Calculator.Operation, firstValue : Int, secondValue : Int) in
+            switch operation {
+                case .addition: return self.calculator.add(a: firstValue, b: secondValue)
+                case .subtraction: return self.calculator.subtract(a: firstValue, b: secondValue)
+            }
+            }.map { (resultInt: Int) in
+                return String(resultInt)
+        }
+        
+        signObservable.bindTo(operationLabel.rx.text).addDisposableTo(disposeBag)
+        resultObservable.bindTo(resultLabel.rx.text).addDisposableTo(disposeBag)
+    }
+    
 }
 

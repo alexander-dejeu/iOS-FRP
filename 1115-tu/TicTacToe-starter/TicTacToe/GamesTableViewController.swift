b11da5698ad1c0f2c06c794dd024c0ac6b35c194
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-15 12:13:05 -0800

    updated pods and added observable for FRP tableview

---
+++ 1115-tu/TicTacToe-starter/TicTacToe/GamesTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GamesTableViewController.swift
+//  TicTacToe
+//
+//  Created by Alex Dejeu on 11/15/16.
+//  Copyright © 2016 Nikolas Burk. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class GamesTableViewController: UITableViewController {
+
+    let games : Variable<[Board]> = Variable([])
+    let disposeBag = DisposeBag()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = nil
+        tableView.delegate = nil
+        
+        games.asObservable().bindTo(tableView.rx.items(cellIdentifier: "Cell", cellType : UITableViewCell.cell)) { (index :  Int, board : Board, cell : UITableViewCell) in
+            if let winner = board.winner(){
+                cell.textLabel?.text = "The winner is: \(winner.rawValue)"
+            }
+            else{
+                cell.textLabel?.text = "The current turn is: \(board.currentTurn().rawValue)"
+            }
+            }.addDisposableTo(disposeBag)
+    }
+    
+    
+    
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        let destinationController =
+    }
+    
+
+    
+}
